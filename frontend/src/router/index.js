dd5dd5efdd1d5a4c23b182d182216d92e95ddf02
GllandXie <GallandXie@outlook.com>
2024-10-23 14:01:36 +0800

    增加新页面路由

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Login from "@/views/Login.vue";
 import SignUp from "@/views/SignUp.vue";
+import Personal from "@/views/Profile.vue";
+import Timetable from "@/views/Timetable.vue";
 const routes = [
   {
     path: '/',
@@ -18,6 +20,16 @@ const routes = [
     name:'signup',
     component:SignUp,
   },
+  {
+    path:'/profile',
+    name:'personal',
+    component:Personal,
+  },
+  {
+    path:'/timetable',
+    name:'timetable',
+    component:Timetable,
+  }
 ]
 
 const router = createRouter({
