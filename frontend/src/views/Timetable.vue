04aea5cab7f3068e3ed071f34f3376c8291251eb
GllandXie <GallandXie@outlook.com>
2024-10-23 14:00:40 +0800

    课程表组件，用于组成我的课程界面

---
+++ frontend/src/views/Timetable.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="schedule-page">
+        <el-header class="header">
+          <Header/>
+        </el-header>
+      <div class="content">
+        <h1>我的课表</h1>
+        <Timetable />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import Header from '@/components/Header.vue'
+  import Timetable from '@/components/Timetable.vue'
+  
+  export default {
+    name: 'SchedulePage',
+    components: {
+      Header,
+      Timetable
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .schedule-page {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+  
+  .content {
+    flex: 1;
+    padding: 20px;
+  }
+  
+  h1 {
+    margin-bottom: 20px;
+    text-align: center;
+  }
+
+.header{
+  height:10vh;
+  background-color: #597fc1;
+  display: flex;
+  justify-content: space-between;
+  align-items: center; /* Use the background color similar to the image */
+  color: white;
+}
+  </style>
